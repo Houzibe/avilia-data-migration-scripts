098f770dbdd9290ebbad68b50a13dfc35a3dda8d
Houzibe <dhouzibe1@gmail.com>
2026-02-27 13:47:35 +0100

    imported requests python module for API calls

--- scripts_to_load_hcps.py
+++ scripts_to_load_hcps.py
@@ -2,7 +2,13 @@
 # DATA MIGRATION: LOAD HEALTHCARE PROVIDERS
 #-----------------------------------------------------------
 import pandas as pd
+import requests as req
  
 print("=" * 50)
 print("     SCRIPT TO LOAD HEALTHCARE PROVIDERS")
 print("=" * 50)
+
+#Load dataset
+hcps = pd.read_csv("datasets/avilia_hcps_datasets.csv")
+
+
