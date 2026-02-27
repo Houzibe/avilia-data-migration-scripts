926e95e62b972887edde4ae68acbc567cdc03707
Houzibe <dhouzibe1@gmail.com>
2026-02-27 21:39:23 +0100

    load config file

--- scripts_to_load_hcps.py
+++ scripts_to_load_hcps.py
@@ -8,7 +8,14 @@ print("=" * 50)
 print("     SCRIPT TO LOAD HEALTHCARE PROVIDERS")
 print("=" * 50)
 
+#Load Config
+config = pd.read_json("config.json")
+print(f"{config}")
 #Load dataset
 hcps = pd.read_csv("datasets/avilia_hcps_datasets.csv")
+print("Length:", len(hcps))
 
+
+#Define endPoints
+endpoint = "https://api.testing.melone.io"
 
