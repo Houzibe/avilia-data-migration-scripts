34ce7f2060efc8bf625e57c14ac640573ae91969
Houzibe <dhouzibe1@gmail.com>
2026-02-27 13:00:56 +0100

    added python script to load hcps

---
+++ scripts_to_load_hcps.py
@@ -0,0 +1,8 @@
+#-----------------------------------------------------------
+# DATA MIGRATION: LOAD HEALTHCARE PROVIDERS
+#-----------------------------------------------------------
+import pandas as pd
+ 
+print("=" * 50)
+print("     SCRIPT TO LOAD HEALTHCARE PROVIDERS")
+print("=" * 50)
